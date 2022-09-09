21ab39b4fc4ff62bf1770a5318c3f755933a37cf
StarPackEdu <prosperchinedu@gmail.com>
2022-09-09 16:38:42 -0700

    f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- *main-entry
+ *main - Entry point
  *
- *Return : 0 (success)
+ *Return : Always 0 (success)
  */
 int main (void)
 {
-  puts("\"Programming is like building a multilingual puxxle");
+         puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
