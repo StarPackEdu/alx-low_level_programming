9904f41ca860a20ecc20db15baddfb662fcd8f53
StarPackEdu <prosperchinedu@gmail.com>
2022-09-10 06:06:35 -0700

    f

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <stderr>
+#include <stdio.h>
 
 /**
  *Description: ptint various typr of the computer
@@ -12,10 +12,10 @@ long int b;
 long long int c;
 char d;
 float f;
-printf("size of a char: %d byte(s)\n", (unsigned long)sizeof(d));
-printf("size of an int: %d byte(s)\n", (unsigned long)sizeof(a));
-printf("size of a long int: %d byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long long int: %d byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a float: %d byte(s)\n", (unsigned long)sizeof(f));
+printf("size of a char: %d byte(s)\n", (int)sizeof(d));
+printf("size of an int: %d byte(s)\n", (int)sizeof(a));
+printf("size of a long int: %d byte(s)\n", (int)sizeof(b));
+printf("size of a long long int: %d byte(s)\n", (int)sizeof(c));
+ printf("size of a float: %d byte(s)\n", (int)sizeof(f));
 return (0);
 }
