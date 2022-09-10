f0b230f62374a5d3102ce7fc2d9569cd81278f99
StarPackEdu <prosperchinedu@gmail.com>
2022-09-10 05:19:47 -0700

    d

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *
+ *Description: ptint various typr of the computer
+ * and run on
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("size of a char: %d byte(s)\n", (int) sizeof(char));
+printf("sixe of int: %d byte(s)\n", (int) sizeof(int));
+printf("size of a long int: %d byte(s)\n", (int) sizeof(long int));
+printf("size of long long int: %d byte(s)\n", (int) sizeof(long long int));
+printf("size of a float: %d bytre(s)\n", (int) sizeof(float)
+		
+return (0)
+}
