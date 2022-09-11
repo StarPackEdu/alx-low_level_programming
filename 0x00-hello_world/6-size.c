612e2548041cd5a1529ca98226e3dd2e5350148d
StarPackEdu <prosperchinedu@gmail.com>
2022-09-10 17:28:35 -0700

    g

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,10 @@
  */
 int main(void)
 {
-long long quite_long;
- printf("size of a char: %d byte(s)\n", sizeof(char));
+printf("size of a char: %d byte(s)\n", sizeof(char));
 printf("size of an int: %d byte(s)\n", sizeof(int));
 printf("size of a long int: %d byte(s)\n", sizeof(long int));
-printf("size of a long long int: %d byte(s)\n", sizeof(quit_long));
+printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
