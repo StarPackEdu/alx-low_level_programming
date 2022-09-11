e080af8140479e7b537c96af06c9a3b105f4f306
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-09-11 17:30:17 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
-int main (void)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-
+int main(void)
 {
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+		putchar('\n');
+
+	return (0);
 }
