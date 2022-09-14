bfd246e29e736a3339ae5c601363668dfeb512c3
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-09-14 11:32:29 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,22 @@
-#include <main.h>
+#include <stdio.h>
+#include "main.h"
+
 /**
- * main _putchar for printing
- *
- * Return: Always 0 (success)
+ * main - Entry point.
+ * Description: write a program that prints "_putchar" followed by a new line.
+ * Return: Always 0 (Success.)
  */
 int main(void)
 {
-int i;
-char p[] = "_putchar";
-for (i = 0; i < 8; i++)
-{
-_putchar(p[i]);
-}
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
