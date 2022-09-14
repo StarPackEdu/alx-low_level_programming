f78e748e9691150087046b8a36065ae5f3b510c6
StarPackEdu <prosperchinedu@gmail.com>
2022-09-14 03:24:21 -0700

    g

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main _putchar for printing
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i;
+char p[] = "_putchar";
+for (i = 0; i < 8; i++)
+{
+_putchar(p[i]);
+}
+_putchar('\n');
+return (0);
+}
