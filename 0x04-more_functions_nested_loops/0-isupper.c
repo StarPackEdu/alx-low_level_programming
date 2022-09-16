dfef8d1eab490793a517e8a2d385e6b7e2d6aa78
StarPackEdu <prosperchinedu@gmail.com>
2022-09-16 08:00:20 -0700

    g

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,2 @@
-#include "main.h"
-
-/**
- * _isupper - Checks for uppercase characters
- *
- * @c: Number to be checked
- *
- * Return: 1 (Sucess) OR 0 (Failure)
- */
-
-int _isupper(int c)
-{
-	if (c >= 65 && c <= 90)
-		return (1);
-	else
-		return (0);
-}
+the
+tge
