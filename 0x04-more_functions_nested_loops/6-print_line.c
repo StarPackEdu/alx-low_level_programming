b0520e410999a3cbe7f33d3259e8dab74288e6b4
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-09-16 15:17:18 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,3 +1,14 @@
-the
-tgr
-the
+#include "main.h"
+/**
+*print_line - prints a straight line
+*@n:parameter
+*Return:returns nothing
+*/
+void print_line(int n)
+{
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
