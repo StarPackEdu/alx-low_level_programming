bd7203ccaf883e921f8d6db04ca22cf05edf691e
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-09-20 13:25:48 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,31 @@
 #include "main.h"
-
 /**
- * print_rev - reverse string
- *
- * @s: String to be reversed
- *
- * Return: void
- */
-
+* print_rev - prints a string in reverse
+* @s: string to print
+*/
 
 void print_rev(char *s)
 {
-	int i = 0;
+int len = 0, index = 0;
+len = _strlen(s);
 
-	while (s[i])
-		i++;
+for (index = len - 1; index >= 0; index--)
+_putchar(s[index]);
 
-	while (i--)
-		_putchar(s[i]);
+_putchar('\n');
+}
 
-	_putchar('\n');
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
+ */
+int _strlen(char *s)
+{
+int count, inc;
+inc = 0;
+for (count = 0; s[count] != '\0'; count++)
+inc++;
+
+return (inc);
+}
