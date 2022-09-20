15a6e41e44422b599cf511bccf8252c401e74e6d
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-09-20 13:11:59 +0100

    Update 1-swap.c
    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,14 @@
 #include "main.h"
-
 /**
- * reset_to_98 - Directs a pointer to an int and changes
- * and changes the value to 98.
- *
- * @n: The variable pointing to the integer
- *
- * Return: Nothing
- */
+*swap_int - swap two integers
+*@a: first integer
+*@b: second integer
+*Return: nothing
+*/
 
-void reset_to_98(int *n)
+void swap_int(int *a, int *b)
 {
-	*n = 98;
+int tmp = *a;
+*a = *b;
+*b = tmp;
 }
