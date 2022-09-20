740db2ffae9632019b6ba5199d44e2418466a199
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-09-20 13:32:59 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,39 +1,29 @@
 #include "main.h"
+
 /**
-*puts_half - puts half of a string
-*@str: string
-*Return:void
-*/
+ * puts_half - print half a strong
+ *
+ * @str: input string
+ *
+ * Return: void
+ *
+ */
+
 void puts_half(char *str)
 {
-int len = _strlen(str);
+	int i = 0;
+	int n;
 
-if (len % 2 == 0)
-len = len / 2;
+	while (str[i] != '\0')
+		i++;
 
-else
-len = (len + 1) / 2;
+	if (i % 2 != 0)
+		n = (i + 1) / 2;
+	else
+		n = i / 2;
 
-while (str[len] != '\0')
-{
-_putchar(str[len]);
-len++;
-}
-_putchar('\n');
-}
+	for (; str[n] != '\0'; n++)
+		_putchar(str[n]);
 
-
-/**
- *_strlen - returns the length of a string
- * @s: string
- *Return: returns lenght;
- */
-int _strlen(char *s)
-{
-int count, inc;
-inc = 0;
-for (count = 0; s[count] != '\0'; count++)
-inc++;
-
-return (inc);
+	_putchar('\n');
 }
