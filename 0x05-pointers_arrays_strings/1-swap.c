bd26ec12e1debfa48538066d0a42b0be7458fd31
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-09-20 13:04:37 +0100

    Update 1-swap.c
    reset

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,3 +1,15 @@
-trhe
-thr
-mn
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Directs a pointer to an int and changes
+ * and changes the value to 98.
+ *
+ * @n: The variable pointing to the integer
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
