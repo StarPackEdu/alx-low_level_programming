026d103e9ce8694f1895c189140091d867731d17
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-09-22 12:32:40 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,64 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdio.h>
 
 /**
- * cap_string - Write a function that capitalizes all words of a string.
+ * isLower - Determines whether ascii is lowercase
  *
- * @entry: This is the input string
+ * @c: Character
  *
- * Return: String capitalized
+ * Return: 1 if true, 0 if false
  */
-char *cap_string(char *entry)
+
+int isLower(char c)
 {
-	int conversion, index, count;
+	return (c >= 97 && c <= 122);
+}
 
-	char chars[] = {' ', ',', ';', '.', '!',
-			 '?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
-	conversion = 32;
+/**
+ * isDelimiter - Determines whether ascii is a delimiter
+ *
+ * @c: Character
+ *
+ * Return: 1 if true, 0 if false
+ */
 
-	for (index = 0; entry[index] != '\0'; index++)
+int isDelimiter(char c)
+{
+	int i;
+	char delimiter[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (1);
+
+	return (0);
+}
+
+/**
+ * cap_string - Capitalizes all words of a string
+ *
+ * @s: Input string
+ *
+ * Return: string with capitalized words
+ */
+
+char *cap_string(char *s)
+{
+	char *ptr = s;
+	int foundDelimit = 1;
+
+	while (*s)
 	{
-		if (entry[index] >= 'index' && entry[index] <= 'z')
+		if (isDelimiter(*s))
+			foundDelimit = 1;
+		else if (isLower(*s) && foundDelimit)
 		{
-			entry[index] =  entry[index] - conversion;
+			*s -= 32;
+			foundDelimit = 0;
 		}
-		conversion = 0;
-		for (count = 0; chars[count] != '\0'; count++)
-		{
-			if (chars[count] == entry[index])
-			{
-				conversion = 32;
-				break;
-			}
-		}
+		else
+			foundDelimit = 0;
+		s++;
 	}
-	return (entry);
+	return (ptr);
 }
