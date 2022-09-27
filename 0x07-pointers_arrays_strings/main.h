ed40dca5d2dcb89e024d22aec25c72cfb0b6f223
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-09-27 12:28:14 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,13 +7,13 @@
  *char prototypes()
  */
 int _putchar(char);
+int _start(char);
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-char *_start(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
