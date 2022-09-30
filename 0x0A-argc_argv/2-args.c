602789f0607426264278fde35fa979a7536842ff
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-09-30 18:02:17 +0100

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,2 +1,18 @@
-tghe11;rgb:0000/0000/0000
-thr
+#include <stdio.h>
+
+/**
+* main - prints all arguments it received
+*@argc: argument count
+*@argv: argument vector
+*Return: returns 0
+*/
+
+int main(int argc, char *argv[])
+{
+		int i;
+
+		for (i = 0; i < argc; i++)
+			printf("%s\n", argv[i]);
+
+		return (0);
+}
