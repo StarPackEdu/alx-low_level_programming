be4ec4784a8eaa78e075c143f9f85965a7700358
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-10-04 11:26:20 +0100

    Update 1-strdup.c

--- 0x0B-mallock_free/1-strdup.c
+++ 0x0B-mallock_free/1-strdup.c
@@ -1,28 +1,38 @@
-#include "main.h"
+#include <stdlib.h>
 
 /**
-* create_array - creates an array of chars, and
-* initializes it with a specific char.
-*@size: Size of the array
-*@c: Character to insert
-*Return: NULL if size is zero or if it fails,
-*pointer to array if everything is normal.
-*/
+ * _strdup - Copies a string to new allocated space in memory
+ *
+ * @str: The string to be copied
+ *
+ * Return: Pointer to allocated memory containing copy of @str (SUCCESS)
+ * OR NULL if @str is empty (FAILURE) OR
+ * NUL if insufficient memory was available
+ */
 
-char *create_array(unsigned int size, char c)
+char *_strdup(char *str)
 {
-	char *array;
-	unsigned int index;
+	char *alloc_mem;
+	unsigned int i;
+	unsigned int length_of_string = 0;
 
-	if (size == 0)
-		return (NULL);
-	array = malloc(sizeof(char) * size);
+	if (str == NULL)
+		return ('\0');
 
-	if (array == NULL)
-		return (NULL);
+	while (*(str + length_of_string) != '\0')
+		length_of_string++;
 
-	for (index = 0; index < size; index++)
-		array[index] = c;
+	length_of_string++;
 
-	return (array);
+	alloc_mem = malloc(sizeof(*str) * length_of_string);
+
+	if (alloc_mem == NULL)
+		return ('\0');
+
+	for (i = 0; i < length_of_string; i++)
+		alloc_mem[i] = *(str + i);
+
+	alloc_mem[i] = '\0';
+
+	return (alloc_mem);
 }
