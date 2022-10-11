7fb349fe0f46a6ddd0ddb2c7f2008c83c0c3fc0c
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-10-11 03:34:00 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,2 +1,14 @@
-thr
-yh
+#include <stdio.h>
+
+/**
+*main - prints the name of
+*the file it was compiled from
+*
+*Return: nothing to be returned
+*/
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
