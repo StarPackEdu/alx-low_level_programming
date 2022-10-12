37844931316d37403625211a3c2b224a2313f5b3
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-10-13 00:16:58 +0100

    Update 0-main.c

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -6,16 +6,9 @@
  *
  * Return: Always 0.
  */
-struct dog 
-{
-char *name;
-    float age;
-    char *owner;
-};
 int main(void)
 {
     struct dog my_dog;
-    
     my_dog.name = "Poppy";
     my_dog.age = 3.5;
     my_dog.owner = "Bob";
