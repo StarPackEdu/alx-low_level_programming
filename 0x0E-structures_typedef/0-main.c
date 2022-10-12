177ff864bbe29ccfd522c41538a8934a81d96372
StarPackEdu <111011755+StarPackEdu@users.noreply.github.com>
2022-10-13 00:01:43 +0100

    Update 0-main.c

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -11,13 +11,9 @@ struct dog
 char *name;
     float age;
     char *owner;
-};
+}
 int main(void)
 {
     struct dog my_dog;
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    printf("My name is %c, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
     return (0);
 }
